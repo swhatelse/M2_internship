ba053475eb608c6d79bdae10cc34745e1f89b34b
swhatelse <stevenwhatelse@hotmail.fr>
2016-04-25 14:36:17 +0200

    Corrected

--- scripts/bruteforce.sh
+++ scripts/bruteforce.sh
@@ -3,26 +3,29 @@
 help_script()
 {
     cat << EOF
-    Usage: $0 options
+Usage: $0 options
 
-    Script for running StarPU with benchmarking
+Script for running StarPU with benchmarking
 
-    OPTIONS:
-       -h      Show this message
-       -c      Specify the size of the checkpoint
-       -d      Detailed data file for a new experiment
-       -i      Specify the OpenCL implementation to use (NVIDIA, Intel)
-       -s      Specify a seed
-       -r      Reuse a detailed data file to complete the experiment by specifying the path of the file
-    EOF
+OPTIONS:
+  -h      Show this message
+  -c      Specify the size of the checkpoint
+  -d      Detailed data file for a new experiment
+  -i      Specify the OpenCL implementation to use (NVIDIA, Intel)
+  -n      Run a new experiment
+  -s      Specify a seed
+  -r      Reuse a detailed data file to complete the experiment by specifying the path of the file
+EOF
 }
 
 DATA_FILE_DETAILED=""
 SEED=""
 CHECKPOINT_SIZE=""
 REUSE_FILE=""
+NEW=false
+part=1
 
-while getopts "c:d:hs:" opt; do
+while getopts "c:d:hi:nr:s:" opt; do
     case $opt in
         c)
             CHECKPOINT_SIZE="$OPTARG"
@@ -36,6 +39,9 @@ while getopts "c:d:hs:" opt; do
             ;;
         i)
             IMPLEM="$OPTARG"
+            ;;
+        n)
+            NEW=true
             ;;
         s)
             SEED="$OPTARG"
@@ -58,14 +64,15 @@ DATADIR="$BASE/data"
 if [[ $IMPLEM == "NVIDIA" ]];
 then
     CMD="CLPLATFORM=NVIDIA"
-elif[[$IMPLEM == "INTEL"]];
-then 
+elif [[ $IMPLEM == "INTEL" ]];
+then
     CMD="CLPLATFORM=Intel"
 fi
 
 CMD="$CMD VERBOSE=true ruby Laplacian.rb -c"
 
-if [[ $DATA_FILE_DETAILED != "" ]];
+# Create or complete the experiment
+if [[ $NEW = true ]];
 then
     # Configuration of new files and and folders path
     DATA_FOLD_DAY=`date +%Y_%m_%d`
@@ -81,24 +88,22 @@ then
     INFO_NAME="Info"
     DATA_FILE="Data"
     INFO_FILE="$DATA_FOLD_TIME/$INFO_NAME${BKUP}.org"
-    DATA_FILE="$DATA_FILE${BKUP}"
-
-    if [[ -f $DATA_FILE_DETAILED ]] ; 
-    then
-        CMD="$CMD -l $DATA_FILE_DETAILED -r"
-    else
-        CMD="$CMD -l $DATA_FILE_DETAILED"
-    fi
+    DATA_FILE="$DATA_FOLD_TIME/$DATA_FILE${BKUP}.yaml"
+    CMD="$CMD -l $DATA_FILE"
+    echo "Creating a new experiment at $DATA_FOLD_TIME"
 elif [[ $REUSE_FILE != "" ]];
 then
-    DATA_FOLD_TIME=`dirname($REUSE_FILE)`
-    INFO_FILE=`find . -type f -iname "Info*"`
-    part=0
-    while [ -e $REUSE_FILE${part}.org ]; do 
+    REUSE_FILE=`readlink -f $REUSE_FILE`
+    PART_NAME=`basename $REUSE_FILE | cut -f 1 -d '.'`
+    DATA_FOLD_TIME=`dirname $(readlink -f $REUSE_FILE)`
+    INFO_FILE=`find $DATA_FOLD_TIME -type f -iname "Info*"`
+    while [ -e $PART_NAME${part}.yaml ]; do 
         bkup=`expr $part + 1`
     done
-    cp $REUSE_FILE $REUSE_FILE${part}
+    COPY="$DATA_FOLD_TIME/${PART_NAME}_part${part}.yaml"
+    cp $REUSE_FILE $COPY
     CMD="$CMD -l $REUSE_FILE -r"
+    echo "Completing experiment at $DATA_FOLD_TIME"
 fi
 
 if [[ $SEED != "" ]] ;
@@ -114,7 +119,7 @@ fi
 CMD="$CMD >> $INFO_FILE"
 
 ######### Collecting informations about the platform #########
-if [[ $REUSE_PATH == "" ]];
+if [[ $NEW = true ]];
 then
     echo "#+TITLE: Experiment information" >> $INFO_FILE
     echo "#+DATE: $(eval date)" >> $INFO_FILE
@@ -183,17 +188,20 @@ then
     echo "#+BEGIN_EXAMPLE" >> $INFO_FILE
     who >> $INFO_FILE
     echo "#+END_EXAMPLE" >> $INFO_FILE
-fi
 
-####### BOAST installation #######
-cd $BASE/boast
-gem build *.gemspec
-gem install --user-install --no-rdoc --no-ri *.gem
+    ####### BOAST installation #######
+    cd $BASE/boast
+    gem build *.gemspec
+    gem install --user-install --no-rdoc --no-ri *.gem
+fi
 
 ######## Run experiment ########
 cd $BASE/boast-lig/ARMclbench
+if [[ $NEW = true ]];
+then
+    part=0
+fi
 echo "* PROGRAM OUTPUT" >> $INFO_FILE
-# TODO add experiment number
 echo "** Part $part" >> $INFO_FILE
 echo "#+begin_src sh :results output :exports both" >> $INFO_FILE
 echo $CMD >> $INFO_FILE    
@@ -201,5 +209,3 @@ echo "#+end_src" >> $INFO_FILE
 echo "#+BEGIN_EXAMPLE" >> $INFO_FILE    
 eval $CMD
 echo "#+END_EXAMPLE" >> $INFO_FILE
-# mv $DATA_FILE'.yaml' $DATA_FOLD_TIME
-# mv $DATA_FILE'_parameters.yaml' $DATA_FOLD_TIME
