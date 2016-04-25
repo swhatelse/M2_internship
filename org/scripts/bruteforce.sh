ccbf2b889f548103cf9486041815aa82de07a6af
swhatelse <stevenwhatelse@hotmail.fr>
2016-04-25 11:41:37 +0200

    U

--- org/scripts/bruteforce.sh
+++ org/scripts/bruteforce.sh
@@ -10,15 +10,17 @@ help_script()
     OPTIONS:
        -h      Show this message
        -c      Specify the size of the checkpoint
-       -d      Detailed data file
+       -d      Detailed data file for a new experiment
        -i      Specify the OpenCL implementation to use (NVIDIA, Intel)
        -s      Specify a seed
+       -r      Reuse a detailed data file to complete the experiment by specifying the path of the file
     EOF
 }
 
 DATA_FILE_DETAILED=""
 SEED=""
 CHECKPOINT_SIZE=""
+REUSE_FILE=""
 
 while getopts "c:d:hs:" opt; do
     case $opt in
@@ -29,7 +31,7 @@ while getopts "c:d:hs:" opt; do
             DATA_FILE_DETAILED="$OPTARG"
             ;;
         h)
-            # help_script
+            help_script
             exit 4
             ;;
         i)
@@ -38,9 +40,12 @@ while getopts "c:d:hs:" opt; do
         s)
             SEED="$OPTARG"
             ;;
+        r)
+            REUSE_FILE="$OPTARG"
+            ;;
         \?)
             echo "Invalid option: -$OPTARG"
-            # help_script
+            help_script
             exit 3
             ;;
     esac
@@ -48,22 +53,7 @@ done
 
 
 BASE="$PWD/.."
-
 DATADIR="$BASE/data"
-DATA_FOLD_DAY=`date +%Y_%m_%d`
-DATA_FOLD_DAY="$DATADIR/$DATA_FOLD_DAY"
-BKUP=`date +%H_%M_%S`
-DATA_FOLD_HOST=`hostname`
-DATA_FOLD_HOST="$DATA_FOLD_DAY/$DATA_FOLD_HOST"
-DATA_FOLD_TIME="$DATA_FOLD_HOST/$BKUP"
-mkdir -p $DATADIR
-mkdir -p $DATA_FOLD_DAY
-mkdir -p $DATA_FOLD_HOST
-mkdir -p $DATA_FOLD_TIME
-INFO_NAME="Info"
-DATA_FILE="Data"
-INFO_FILE="$DATA_FOLD_TIME/$INFO_NAME${BKUP}.org"
-DATA_FILE="$DATA_FILE${BKUP}"
 
 if [[ $IMPLEM == "NVIDIA" ]];
 then
@@ -74,14 +64,41 @@ then
 fi
 
 CMD="$CMD VERBOSE=true ruby Laplacian.rb -c"
+
 if [[ $DATA_FILE_DETAILED != "" ]];
 then
+    # Configuration of new files and and folders path
+    DATA_FOLD_DAY=`date +%Y_%m_%d`
+    DATA_FOLD_DAY="$DATADIR/$DATA_FOLD_DAY"
+    BKUP=`date +%H_%M_%S`
+    DATA_FOLD_HOST=`hostname`
+    DATA_FOLD_HOST="$DATA_FOLD_DAY/$DATA_FOLD_HOST"
+    DATA_FOLD_TIME="$DATA_FOLD_HOST/$BKUP"
+    mkdir -p $DATADIR
+    mkdir -p $DATA_FOLD_DAY
+    mkdir -p $DATA_FOLD_HOST
+    mkdir -p $DATA_FOLD_TIME
+    INFO_NAME="Info"
+    DATA_FILE="Data"
+    INFO_FILE="$DATA_FOLD_TIME/$INFO_NAME${BKUP}.org"
+    DATA_FILE="$DATA_FILE${BKUP}"
+
     if [[ -f $DATA_FILE_DETAILED ]] ; 
     then
-        CMD="$CMD -l $DATA_FILE_DETAILED -r >> $INFO_FILE"
+        CMD="$CMD -l $DATA_FILE_DETAILED -r"
     else
-        CMD="$CMD -l $DATA_FILE_DETAILED >> $INFO_FILE"
+        CMD="$CMD -l $DATA_FILE_DETAILED"
     fi
+elif [[ $REUSE_FILE != "" ]];
+then
+    DATA_FOLD_TIME=`dirname($REUSE_FILE)`
+    INFO_FILE=`find . -type f -iname "Info*"`
+    part=0
+    while [ -e $REUSE_FILE${part}.org ]; do 
+        bkup=`expr $part + 1`
+    done
+    cp $REUSE_FILE $REUSE_FILE${part}
+    CMD="$CMD -l $REUSE_FILE -r"
 fi
 
 if [[ $SEED != "" ]] ;
@@ -97,73 +114,76 @@ fi
 CMD="$CMD >> $INFO_FILE"
 
 ######### Collecting informations about the platform #########
-echo "#+TITLE: Experiment information" >> $INFO_FILE
-echo "#+DATE: $(eval date)" >> $INFO_FILE
-echo "#+MACHINE: $(eval hostname)" >> $INFO_FILE
-echo "#+FILE: $INFO_FILE" >> $INFO_FILE
-
-echo "* ENVIRONMENT INFOS" >> $INFO_FILE
-
-echo "** HARDWARE" >> $INFO_FILE
-
-echo "*** CPU" >> $INFO_FILE
-echo "#+BEGIN_EXAMPLE" >> $INFO_FILE
-less /proc/cpuinfo >> $INFO_FILE
-echo "#+END_EXAMPLE" >> $INFO_FILE
-
-if [[ -n $(command -v nvidia-smi) ]];
+if [[ $REUSE_PATH == "" ]];
 then
-    echo "*** GPU INFO FROM NVIDIA-SMI" >> $INFO_FILE
-    echo "#+BEGIN_EXAMPLE" >> $INFO_FILE    
-    nvidia-smi -q >> $INFO_FILE
-    echo "#+END_EXAMPLE" >> $INFO_FILE
-else
-    echo "*** GPU" >> $INFO_FILE
+    echo "#+TITLE: Experiment information" >> $INFO_FILE
+    echo "#+DATE: $(eval date)" >> $INFO_FILE
+    echo "#+MACHINE: $(eval hostname)" >> $INFO_FILE
+    echo "#+FILE: $INFO_FILE" >> $INFO_FILE
+
+    echo "* ENVIRONMENT INFOS" >> $INFO_FILE
+
+    echo "** HARDWARE" >> $INFO_FILE
+
+    echo "*** CPU" >> $INFO_FILE
     echo "#+BEGIN_EXAMPLE" >> $INFO_FILE
-    lshw -numeric -C display >> $INFO_FILE
+    less /proc/cpuinfo >> $INFO_FILE
     echo "#+END_EXAMPLE" >> $INFO_FILE
-fi 
 
-echo "** SOFTWARE" >> $INFO_FILE
+    if [[ -n $(command -v nvidia-smi) ]];
+    then
+        echo "*** GPU INFO FROM NVIDIA-SMI" >> $INFO_FILE
+        echo "#+BEGIN_EXAMPLE" >> $INFO_FILE    
+        nvidia-smi -q >> $INFO_FILE
+        echo "#+END_EXAMPLE" >> $INFO_FILE
+    else
+        echo "*** GPU" >> $INFO_FILE
+        echo "#+BEGIN_EXAMPLE" >> $INFO_FILE
+        lshw -numeric -C display >> $INFO_FILE
+        echo "#+END_EXAMPLE" >> $INFO_FILE
+    fi 
 
-if [ -f /proc/version ];
-then
-    echo "*** LINUX AND GCC VERSIONS" >> $INFO_FILE
-    echo "#+BEGIN_EXAMPLE" >> $INFO_FILE    
-    cat /proc/version >> $INFO_FILE
+    echo "** SOFTWARE" >> $INFO_FILE
+
+    if [ -f /proc/version ];
+    then
+        echo "*** LINUX AND GCC VERSIONS" >> $INFO_FILE
+        echo "#+BEGIN_EXAMPLE" >> $INFO_FILE    
+        cat /proc/version >> $INFO_FILE
+        echo "#+END_EXAMPLE" >> $INFO_FILE
+    fi
+
+    echo "*** ENVIRONMENT VARIABLES"  >> $INFO_FILE
+    echo "#+BEGIN_EXAMPLE" >> $INFO_FILE
+    env >> $INFO_FILE
+    echo "#+END_EXAMPLE" >> $INFO_FILE
+
+    if [ -f /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor ];
+    then
+        echo "*** CPU GOVERNOR" >> $INFO_FILE
+        echo "#+BEGIN_EXAMPLE" >> $INFO_FILE    
+        cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor >> $INFO_FILE
+        echo "#+END_EXAMPLE" >> $INFO_FILE
+    fi
+
+    if [ -f /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq ];
+    then
+        echo "*** CPU FREQUENCY" >> $INFO_FILE
+        echo "#+BEGIN_EXAMPLE" >> $INFO_FILE    
+        cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq >> $INFO_FILE
+        echo "#+END_EXAMPLE" >> $INFO_FILE
+    fi
+
+    echo "*** SOFTWARES RUNNING"  >> $INFO_FILE
+    echo "#+BEGIN_EXAMPLE" >> $INFO_FILE
+    ps -le >> $INFO_FILE
+    echo "#+END_EXAMPLE" >> $INFO_FILE
+
+    echo "*** USERS USING THE SYSTEM"  >> $INFO_FILE
+    echo "#+BEGIN_EXAMPLE" >> $INFO_FILE
+    who >> $INFO_FILE
     echo "#+END_EXAMPLE" >> $INFO_FILE
 fi
-
-echo "*** ENVIRONMENT VARIABLES"  >> $INFO_FILE
-echo "#+BEGIN_EXAMPLE" >> $INFO_FILE
-env >> $INFO_FILE
-echo "#+END_EXAMPLE" >> $INFO_FILE
-
-if [ -f /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor ];
-then
-    echo "*** CPU GOVERNOR" >> $INFO_FILE
-    echo "#+BEGIN_EXAMPLE" >> $INFO_FILE    
-    cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor >> $INFO_FILE
-    echo "#+END_EXAMPLE" >> $INFO_FILE
-fi
-
-if [ -f /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq ];
-then
-    echo "*** CPU FREQUENCY" >> $INFO_FILE
-    echo "#+BEGIN_EXAMPLE" >> $INFO_FILE    
-    cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq >> $INFO_FILE
-    echo "#+END_EXAMPLE" >> $INFO_FILE
-fi
-
-echo "*** SOFTWARES RUNNING"  >> $INFO_FILE
-echo "#+BEGIN_EXAMPLE" >> $INFO_FILE
-ps -le >> $INFO_FILE
-echo "#+END_EXAMPLE" >> $INFO_FILE
-
-echo "*** USERS USING THE SYSTEM"  >> $INFO_FILE
-echo "#+BEGIN_EXAMPLE" >> $INFO_FILE
-who >> $INFO_FILE
-echo "#+END_EXAMPLE" >> $INFO_FILE
 
 ####### BOAST installation #######
 cd $BASE/boast
@@ -173,11 +193,13 @@ gem install --user-install --no-rdoc --no-ri *.gem
 ######## Run experiment ########
 cd $BASE/boast-lig/ARMclbench
 echo "* PROGRAM OUTPUT" >> $INFO_FILE
+# TODO add experiment number
+echo "** Part $part" >> $INFO_FILE
 echo "#+begin_src sh :results output :exports both" >> $INFO_FILE
 echo $CMD >> $INFO_FILE    
 echo "#+end_src" >> $INFO_FILE
 echo "#+BEGIN_EXAMPLE" >> $INFO_FILE    
 eval $CMD
 echo "#+END_EXAMPLE" >> $INFO_FILE
-mv $DATA_FILE'.yaml' $DATA_FOLD_TIME
-mv $DATA_FILE'_parameters.yaml' $DATA_FOLD_TIME
+# mv $DATA_FILE'.yaml' $DATA_FOLD_TIME
+# mv $DATA_FILE'_parameters.yaml' $DATA_FOLD_TIME
