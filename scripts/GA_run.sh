15bc65ac7dc17411d02d38b90463c94c6d745607
swhatelse <stevenwhatelse@hotmail.fr>
2016-06-05 11:14:48 +0200

    U

--- scripts/GA_run.sh
+++ scripts/GA_run.sh
@@ -1,6 +1,6 @@
 
 DAY=`date +%Y_%m_%d`
-DIR="../data/$DAY"
+DIR="$PWD/../data/$DAY"
 mkdir -p $DIR
 DIR="$DIR/pilipili2"
 mkdir -p $DIR
@@ -8,9 +8,9 @@ DIR="$DIR/algos"
 mkdir -p $DIR
 DIR="$DIR/GA"
 mkdir -p $DIR
-INPUT="../data/2016_04_08/pilipili2/18_08_24/test_space_2016_04_02_end_cleaned.yaml"
-PARAM="../data/2016_04_08/pilipili2/18_08_24/test_space_2016_04_02_parameters_v2.yaml"
+INPUT="$PWD/../data/2016_04_08/pilipili2/18_08_24/test_space_2016_04_02_end_cleaned.yaml"
+PARAM="$PWD/../data/2016_04_08/pilipili2/18_08_24/test_space_2016_04_02_parameters_v2.yaml"
 
 for i in `seq 1 100`; do 
-  ../scripts/GA_experiment.sh $INPUT $PARAM ${DIR}/"$i.yaml" &
+    ruby ../boast/optimizer_benchmarks/bench_gen.rb $PARAM $INPUT -r ${DIR}/"$i.yaml" --generations_limit 5 --population_size 20 --mutation_rate 0.1 --elitism 1 --twin_removal true > /dev/null &
 done
